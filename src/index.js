f39d89949300a99b13caa4ea4c07ba008aac9758
DanielOgbalidet <Danielogbalidet@gmail.com>
2022-11-07 17:24:46 +0100

    Code will now take time for input in milliseconds

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+let time = false;
+let timeStart;
+let timeEnd;
+let timeTotal;
+
+let time2 = false;
+let timeStart2;
+let timeEnd2;
+let timeTotal2;
+
 function change() {
     let top = $("#top").val();
-    //let last = top % 10;
     let last = top.charAt(top.length - 1);
     let number = top.substring(0, top.length - 1);
 
@@ -22,6 +31,34 @@ function change() {
     else if(last === "9") {
         last = 3;
     }
+    if(!time) {
+        timeStart = (new Date).getTime();
+        time = true;
+    }
+    if(last === "0") {
+        timeEnd = (new Date).getTime();
+        timeTotal = timeEnd - timeStart;
+        $("#total").html(timeTotal);
+    }
 
     $("#top").val(number + last);
 }
+
+function timeSecond() {
+    let top = $("#top").val();
+    let last = top.charAt(top.length - 1);
+    let number = top.substring(0, top.length - 1);
+
+    if(!time2) {
+        timeStart2 = (new Date).getTime();
+        time2 = true;
+    }
+    if(last === "0") {
+        timeEnd2 = (new Date).getTime();
+        timeTotal2 = timeEnd2 - timeStart2;
+        $("#total2").html(timeTotal2);
+    }
+
+    $("#top").val(number + last);
+}
+
