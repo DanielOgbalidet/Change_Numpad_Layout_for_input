6369081812ac3086349d10e13a646fb8cdb4a561
DanielOgbalidet <Danielogbalidet@gmail.com>
2022-11-07 16:44:00 +0100

    Fixed code for numbers with more than 16 digits

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 function change() {
     let top = $("#top").val();
-    let last = top % 10;
-    let number = top.slice(0, -1);
+    //let last = top % 10;
+    let last = top.charAt(top.length - 1);
+    let number = top.substring(0, top.length - 1);
 
-    if(last === 1) {
+    if(last === "1") {
         last = 7;
     }
-    else if(last === 2) {
+    else if(last === "2") {
         last = 8;
     }
-    else if(last === 3) {
+    else if(last === "3") {
         last = 9;
     }
-    else if(last === 7) {
+    else if(last === "7") {
         last = 1;
     }
-    else if(last === 8) {
+    else if(last === "8") {
         last = 2;
     }
-    else if(last === 9) {
+    else if(last === "9") {
         last = 3;
     }
 
